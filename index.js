70660dbab6d851bc90d496b009543a49236ef11b
Simon <ximengyu012@gmail.com>
2025-04-28 17:25:27 -0700

    google oauth

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT);
