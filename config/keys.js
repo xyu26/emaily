bdd25c92cf97950efcf79f3542a71f3b14a58ae2
Simon <ximengyu012@gmail.com>
2025-05-02 09:38:31 -0700

    clean up

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = 
-{
-	googleClientID:"923051162849-7cv19ac4assg52g0suftmgvammv80e7p.apps.googleusercontent.com", 
-	
-	googleClientSecret:"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",
-
-	mongoURI: 'mongodb+srv://ximengyu012:670jQ2FqCkbutEtp@cluster0.wdfn1ck.mongodb.net/emailydb?retryWrites=true&w=majority&appName=Cluster0'
-};
